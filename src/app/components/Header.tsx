0fe41beca66e1ab0fcf508f6e11db6a5cc67857c
Samuel Chen <sche842@gmail.com>
2025-02-21 22:42:12 +1100

    header styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -7,8 +8,8 @@ const navLinks = [
     href: "/",
   },
   {
-    name: "projects",
-    href: "/projects",
+    name: "about",
+    href: "/about",
   },
   {
     name: "contact",
@@ -17,13 +18,22 @@ const navLinks = [
 ];
 
 const Header = () => {
+  const path = usePathname();
+
   return (
     <header className="sticky top-0 z-50 bg-background/75 py-6 backdrop-blur-sm">
       <nav className="flex items-center justify-between">
         <ul className="flex gap-4 sm:gap-8">
           {navLinks.map((nav, id) => (
-            <li key={id} className="link">
-              <Link href={nav.href}>{nav.name}</Link>
+            <li key={id} className="">
+              <Link
+                href={nav.href}
+                className={
+                  path === `${nav.href}` ? "text-red-500" : "text-blue-500"
+                }
+              >
+                {nav.name}
+              </Link>
             </li>
           ))}
         </ul>
